8a823d12678f7c56e658771987fc9fcc1a4e8685
gene-gw <gene.nomicos@graduway.com>
2022-01-26 19:43:25 -0800

    add section admin email to matching gift sections

--- app/models/matching-gifts-section.js
+++ app/models/matching-gifts-section.js
@@ -5,6 +5,13 @@ const Validations = buildValidations({
   name: validator('presence', true),
   campaign_id: validator('presence', true),
   website_id: validator('presence', true),
+  email_failed_charge: [
+    validator('presence', true),
+    validator('format', {
+      regex: /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/,
+      message: 'Please enter a valid email address'
+    })
+  ],
 });
 
 export default DS.Model.extend(Validations, {
@@ -12,6 +19,7 @@ export default DS.Model.extend(Validations, {
   campaign_name: DS.attr('string'),
   name: DS.attr('string'),
   match_selection_strategy: DS.attr('string'),
+  email_failed_charge: DS.attr('string'),
   website_id: DS.attr('string'),
   matchingGiftsSectionMappings: DS.hasMany('matching-gifts-section-mapping'),
 });
