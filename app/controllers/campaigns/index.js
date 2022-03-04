410c033203a55c943e390c478387e02c60c57456
Omri8704 <omri.vaknin@graduway.com>
2022-03-04 09:27:26 +0200

    Fix sreach in campaign & m.gift dashboard

--- app/controllers/campaigns/index.js
+++ app/controllers/campaigns/index.js
@@ -32,12 +32,16 @@ export default Controller.extend({
     return this.get("settings.current_entity.can_create_campaign")
   }),
 
-  _setSearchTerm: Ember.observer('search_term_debounce', function() {
+  _setSearchTerm: Ember.observer('search_term_debounce', function () {
     let timer = this.get("debounce_timer")
     if( timer ) Ember.run.cancel( timer )
-    this.set("debounce_timer", Ember.run.debounce(this, function(){
+    this.set("debounce_timer", Ember.run.debounce(this, function () {
+      if (this.get('search_term_debounce') && this.get('search_term_debounce').length > 0
+        && this.get('search_term') != this.get('search_term_debounce')) {
+        this.set("offset", 0)
+      }
       this.set('search_term', this.get('search_term_debounce'));
-    }, 400 ))
+    }, 800 ))
   }),
 
   actions: {
