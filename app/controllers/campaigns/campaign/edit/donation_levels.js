fe2a03e70387994e1aa54f241af8042ce29b010e
gene-gw <gene.nomicos@graduway.com>
2022-09-01 15:52:18 -0700

    incentive levels

--- app/controllers/campaigns/campaign/edit/donation_levels.js
+++ app/controllers/campaigns/campaign/edit/donation_levels.js
@@ -12,20 +12,20 @@ export default Ember.Controller.extend({
       donationLevel.save()
         .then( () => {
           this.set("newDonationLevel", campaign.store.createRecord("donation_level"))
-          this.get("notify").success("Donation Level saved.")
+          this.get("notify").success("Incentive Level saved.")
         }).catch((err)=>{
           donationLevel.set("campaign",false)
         })
     },
     deleteDonationLevel(donationLevel){
       donationLevel.destroyRecord().then( ()=> {
-        this.get("notify").success("Donation Level deleted.")
+        this.get("notify").success("Incentive Level deleted.")
       })
     },
     saveCampaign(donationLevel){
       donationLevel.save()
         .then( () => {
-          this.get("notify").success("Donation Level updated.")
+          this.get("notify").success("Incentive Level updated.")
         }).catch( (err) => {
         })
     }
